60930f635463d7d22f5996f05d58848bbcdf8243
Mat Strand <mstrand@23andme.com>
2016-01-29 14:56:40 -0800

    add requests[security]

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     license='MIT',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['requests'],
+    install_requires=['requests', 'requests[security]'],
     classifiers=[
         #'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
