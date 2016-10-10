1f27a25e7efabd92c3f97bbec8b8136578c4a20a
Mat Strand <mstrand@23andme.com>
2016-10-10 16:16:53 -0700

    Add chr6_mcf_hap5 test

--- seqseek/tests/build_specific_tests/build_37_tests.py
+++ seqseek/tests/build_specific_tests/build_37_tests.py
@@ -14,7 +14,7 @@ class TestBuild37(TestCase):
 
     def test_file_count(self):
         file_count = len(fnmatch.filter(os.listdir(TestBuild37.GRCH37_PATH), '*.fa'))
-        self.assertEqual(file_count, 28)
+        self.assertEqual(file_count, 29)
 
     def test_file_names(self):
         for name in BUILD37_CHROMOSOMES.keys():
@@ -26,7 +26,7 @@ class TestBuild37(TestCase):
     # chromosome browser tool
 
     def test_chr_start_sequences(self):
-        exclude = ('MT', '17' , '6_cox_hap2', '6_apd_hap1', '6_ssto_hap7')
+        exclude = ('MT', '17' , '6_cox_hap2', '6_apd_hap1', '6_ssto_hap7', '6_mcf_hap5')
         test_str = "N" * 20
         for name in BUILD37_CHROMOSOMES.keys():
             # these chromosomes do not have telomeres
@@ -199,4 +199,9 @@ class TestBuild37(TestCase):
         expected = "ggcc"
         seq = Chromosome('6_ssto_hap7').sequence(BUILD37_CHROMOSOMES['6_ssto_hap7'] - 4,
                                                 BUILD37_CHROMOSOMES['6_ssto_hap7'])
+        self.assertEqual(expected, seq)
+
+    def test_chr6_mcf_hap5(self):
+        expected = "ACAATTAGAAATACTAGGAG"
+        seq = Chromosome('6_mcf_hap5').sequence(3000, 3020)
         self.assertEqual(expected, seq)
