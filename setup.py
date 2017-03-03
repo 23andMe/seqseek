a43726d41188c91816906ee6a800e3ce3e4eb04b
Mat Strand <mstrand@23andme.com>
2017-03-02 19:14:27 -0800

    bump version

--- setup.py
+++ setup.py
@@ -15,9 +15,9 @@ elif PY3:
 
 setup(
     name='seqseek',
-    version='0.3.1',
+    version='0.3.2',
     url='https://github.com/23andMe/seqseek',
-    download_url = 'https://github.com/23andMe/seqseek/tarball/0.3.1',
+    download_url = 'https://github.com/23andMe/seqseek/tarball/0.3.2',
     author='23andMe Engineering',
     author_email=['mstrand@23andme.com'],
     description='Easy access to human reference genome sequences',
