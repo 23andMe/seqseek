b0d1bb7713f4e0c009a3fcbfcc08650f59d65abc
Mat Strand <mstrand@23andme.com>
2017-03-02 18:21:10 -0800

    update readme and setup

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     url='https://github.com/23andMe/seqseek',
     download_url = 'https://github.com/23andMe/seqseek/tarball/0.3.1',
     author='23andMe Engineering',
-    author_email=['mstrand@23anmde.com'],
+    author_email=['mstrand@23andme.com'],
     description='Easy access to Build 37 & 38 human reference sequences',
     long_description=readme,
     entry_points={'console_scripts': [
