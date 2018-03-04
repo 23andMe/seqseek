ce172e03582edda2805601d71e85ffb965ff7bad
Gökcen Eraslan <gokcen.eraslan@gmail.com>
2018-03-04 04:19:45 +0100

    Add Python3 support

--- seqseek/__init__.py
+++ seqseek/__init__.py
@@ -1,4 +1,5 @@
+from __future__ import absolute_import
 
-from chromosome import Chromosome
+from .chromosome import Chromosome
 
-from lib import BUILD37, BUILD38
+from .lib import BUILD37, BUILD38
--- seqseek/tests/build_specific_tests/build_specific_tests.py
+++ seqseek/tests/build_specific_tests/build_specific_tests.py
@@ -1,7 +1,9 @@
+from __future__ import print_function
+from __future__ import absolute_import
 import unittest
 from seqseek.lib import BUILD37, BUILD38
-from build_37_tests import TestBuild37
-from build_38_tests import TestBuild38
+from .build_37_tests import TestBuild37
+from .build_38_tests import TestBuild38
 
 
 ASSEMBLY_TEST_SUITE = {
@@ -10,7 +12,7 @@ ASSEMBLY_TEST_SUITE = {
 }
 
 def run_build_test_suite(assembly):
-    print "Running tests for {assembly}".format(assembly=assembly)
+    print("Running tests for {assembly}".format(assembly=assembly))
     suite = unittest.TestLoader().loadTestsFromTestCase(ASSEMBLY_TEST_SUITE[assembly])
     unittest.TextTestRunner(verbosity=3).run(suite)
 
